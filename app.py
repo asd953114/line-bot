5d8668a6c926c8e8d5051022d9ecde558506036a
asd953114 <asd953114@gmail.com>
2019-02-27 14:34:33 +0800

    Fix sticker

--- app.py
+++ app.py
@@ -57,11 +57,11 @@ def handle_message(event):
         )
 
         line_bot_api.reply_message(
-        event.reply_token,
-        sticker_message)
+            vent.reply_token,
+            sticker_message)
 
         return
-
+        
     if msg in ["hi", "Hi"]:
         r = "hi!"
     elif msg == "你吃飯了嗎?":
