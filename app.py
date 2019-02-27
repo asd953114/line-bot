e576bba21718ba491752a1cb4c684a3a2a234d82
asd953114 <asd953114@gmail.com>
2019-02-27 14:16:09 +0800

    Fix text version 2

--- app.py
+++ app.py
@@ -49,16 +49,16 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    if msg == "hi":
+    if msg in ["hi", "Hi"]:
         r = "hi"
     elif msg == "你吃飯了嗎":
         r = "還沒"
     else:
-        r = "今天天氣真好呢！"
+        r = "很抱歉，您說什麼?"
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text="r"))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
